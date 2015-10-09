816b44208eddbffff50fead55c92e86ee5aa6444
C.T. Lin <chentsulin@gmail.com>
2015-10-10 02:41:50 +0800

    3-demo: Step 5: Add inverse data flow

--- 3-class-demo/todo.js
+++ 3-class-demo/todo.js
@@ -8,8 +8,53 @@ class TodoApp extends React.Component {
     };
   }
 
-  renderTodoItem(item) {
-    return <TodoItem title={item.title} isCompleted={item.isCompleted} />
+  // 處理 todo 改變的事件
+  handleNewTodoChange(event) {
+    this.setState({ newTodo: event.target.value });
+  }
+
+  // 處理 input 發生鍵盤按鍵按下去的事件
+  handleKeyDown(event) {
+    const inputValue = event.target.value;
+    if (event.keyCode == 13 && inputValue !== '') {
+      const { todos, newTodo } = this.state;
+      // 新增 todo 並清掉 input
+      this.setState({
+        newTodo: '',
+        todos: todos.concat({ title: newTodo, isCompleted: false })
+      });
+    }
+  }
+
+  handleTodoChange(event, i) {
+    const { todos } = this.state;
+    todos.splice(i, 1, {
+      title: todos[i].title,
+      isCompleted: event.target.checked
+    });
+    this.setState({
+      todos: todos
+    });
+  }
+
+  handleTodoDestory(i) {
+    let { todos } = this.state;
+    todos.splice(i, 1);
+    this.setState({
+      todos: todos
+    });
+  }
+
+  renderTodoItem(item, i) {
+    return (
+      <TodoItem
+        index={i}
+        title={item.title}
+        isCompleted={item.isCompleted}
+        onChange={this.handleTodoChange.bind(this)}
+        onDestroy={this.handleTodoDestory.bind(this)}
+      />
+    );
   }
 
   render() {
@@ -24,12 +69,14 @@ class TodoApp extends React.Component {
               placeholder="What needs to be done?"
               autofocus
               value={newTodo}
+              onChange={this.handleNewTodoChange.bind(this)}
+              onKeyDown={this.handleKeyDown.bind(this)}
             />
           </header>
           <section className="main">
             <input className="toggle-all" type="checkbox" />
             <label htmlFor="toggle-all">Mark all as complete</label>
-            <ul className="todo-list">{todos.map(this.renderTodoItem)}</ul>
+            <ul className="todo-list">{todos.map(this.renderTodoItem, this)}</ul>
           </section>
           <footer className="footer">
             <span className="todo-count"></span>
@@ -48,13 +95,18 @@ class TodoApp extends React.Component {
 // todo 項目
 class TodoItem extends React.Component {
   render() {
-    const { title, isCompleted } = this.props;
+    const { index, title, isCompleted, onChange, onDestroy } = this.props;
     return (
       <li>
         <div className="view">
-          <input className="toggle" type="checkbox" checked={isCompleted} />
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={isCompleted}
+            onChange={(event) => onChange(event, index)}
+          />
           <label>{title}</label>
-          <button className="destroy"></button>
+          <button className="destroy" onClcik={(event) => onDestroy(index)}></button>
         </div>
       </li>
     );
